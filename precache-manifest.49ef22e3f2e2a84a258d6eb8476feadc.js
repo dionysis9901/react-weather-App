40179fe45166a22b51d42e0de4d996c2dc16e086
dionysis9901 <dionysis.koufis@gmail.com>
2020-01-20 18:02:22 +0200

    Updates

---
+++ precache-manifest.49ef22e3f2e2a84a258d6eb8476feadc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a67b9f07538b10a5e590a8e3ea974e",
+    "url": "/react-weather-App/index.html"
+  },
+  {
+    "revision": "1ad9079d61b81eaa4356",
+    "url": "/react-weather-App/static/css/main.af2a5266.chunk.css"
+  },
+  {
+    "revision": "9fcd081954bdc982ceb9",
+    "url": "/react-weather-App/static/js/2.cf75b27e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-weather-App/static/js/2.cf75b27e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ad9079d61b81eaa4356",
+    "url": "/react-weather-App/static/js/main.c92ef932.chunk.js"
+  },
+  {
+    "revision": "9b4d6811260efc51290c",
+    "url": "/react-weather-App/static/js/runtime-main.9388e157.js"
+  }
+]);
