8e6278f3a0dadbe5d6a30b573bf56e267f169144
dionysis9901 <dionysis.koufis@gmail.com>
2020-01-20 17:53:31 +0200

    micro changes with the proxy

--- src/components/weatherApp.js
+++ src/components/weatherApp.js
@@ -16,14 +16,19 @@ export default class WeatherApp extends React.Component {
     this.toCelsius = this.toCelsius.bind(this);
   }
   componentDidMount() {
+    document.tittle = "SkyChecker";
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
         this.setState({
           long: position.coords.longitude,
           lat: position.coords.latitude
         });
-        const proxy = "https://cors-anywhere.herokuapp.com/"; // this can be deleted when official launch
-        const api = `${proxy}https://api.darksky.net/forecast/9cf4efbb3abf34df3415d1db93aad9d6/${this.state.lat},${this.state.long}`;
+
+        //  const proxy = "https://cors-anywhere.herokuapp.com/"; // this can be deleted when official launch
+        //const api = `${proxy}https://api.darksky.net/forecast/9cf4efbb3abf34df3415d1db93aad9d6/${this.state.lat},${this.state.long}`;
+        // Use proxy if you want to Run it Locally ;)
+
+        const api = `https://api.darksky.net/forecast/9cf4efbb3abf34df3415d1db93aad9d6/${this.state.lat},${this.state.long}`;
         fetch(api)
           .then(res => {
             return res.json();
